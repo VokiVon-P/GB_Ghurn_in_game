55c3e5ac536589f0405b6ff667ba80b926c6d9b9
Vokivon <vokivon.p@gmail.com>
2020-04-09 13:13:00 +0300

    Подключил logging и сделал его первичную настройку

---
+++ helper/help_logging.py
@@ -0,0 +1,35 @@
+import logging
+import logging.config
+import yaml
+from pprint import pprint
+
+FILE_LOGGING_CFG = '../config/logging_cfg.yaml'
+
+def load_logging_config(filename=None):
+
+    if not filename:
+        filename = FILE_LOGGING_CFG
+
+    with open(filename, 'r') as f:
+        config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+        # pprint(config)
+
+
+def save_logging_config(log_conf: dict, filename=None):
+
+    if not filename:
+        filename = FILE_LOGGING_CFG
+
+    try:
+        with open(filename, 'w') as f_n:
+            yaml.dump(log_conf, f_n, allow_unicode=True, default_flow_style=False)
+
+    except Exception as err:
+        print(f"Ошибка записи файла {filename}\n", err)
+
+
+load_logging_config()
+logger = logging.getLogger('model_logger')
+logger.info('logger configured and started')
+
